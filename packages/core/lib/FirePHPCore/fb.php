dc4fb72fae666e193cd7f8866ed07598578f5e0e
cadorn <christoph@christophdorn.com>
2010-07-18 17:58:01 -0700

    Fixed code indentation

--- packages/core/lib/FirePHPCore/fb.php
+++ packages/core/lib/FirePHPCore/fb.php
@@ -42,7 +42,7 @@
  * @package     FirePHPCore
  */
 
-if(!include_once("FirePHP/Init.php")) {
+if (!include_once("FirePHP/Init.php")) {
     require_once dirname(__FILE__).'/FirePHP.class.php';
 }
 
@@ -58,206 +58,219 @@ if(!include_once("FirePHP/Init.php")) {
  */
 function fb()
 {
-  $instance = FirePHP::getInstance(true);
+    $instance = FirePHP::getInstance(true);
   
-  $args = func_get_args();
-  return call_user_func_array(array($instance,'fb'),$args);
+    $args = func_get_args();
+    return call_user_func_array(array($instance,'fb'),$args);
 }
 
 
 class FB
 {
-  /**
-   * Enable and disable logging to Firebug
-   * 
-   * @see FirePHP->setEnabled()
-   * @param boolean $Enabled TRUE to enable, FALSE to disable
-   * @return void
-   */
-  public static function setEnabled($Enabled) {
-    $instance = FirePHP::getInstance(true);
-    $instance->setEnabled($Enabled);
-  }
+    /**
+     * Enable and disable logging to Firebug
+     * 
+     * @see FirePHP->setEnabled()
+     * @param boolean $Enabled TRUE to enable, FALSE to disable
+     * @return void
+     */
+    public static function setEnabled($Enabled)
+    {
+        $instance = FirePHP::getInstance(true);
+        $instance->setEnabled($Enabled);
+    }
   
-  /**
-   * Check if logging is enabled
-   * 
-   * @see FirePHP->getEnabled()
-   * @return boolean TRUE if enabled
-   */
-  public static function getEnabled() {
-    $instance = FirePHP::getInstance(true);
-    return $instance->getEnabled();
-  }  
+    /**
+     * Check if logging is enabled
+     * 
+     * @see FirePHP->getEnabled()
+     * @return boolean TRUE if enabled
+     */
+    public static function getEnabled()
+    {
+        $instance = FirePHP::getInstance(true);
+        return $instance->getEnabled();
+    }  
   
-  /**
-   * Specify a filter to be used when encoding an object
-   * 
-   * Filters are used to exclude object members.
-   * 
-   * @see FirePHP->setObjectFilter()
-   * @param string $Class The class name of the object
-   * @param array $Filter An array or members to exclude
-   * @return void
-   */
-  public static function setObjectFilter($Class, $Filter) {
-    $instance = FirePHP::getInstance(true);
-    $instance->setObjectFilter($Class, $Filter);
-  }
+    /**
+     * Specify a filter to be used when encoding an object
+     * 
+     * Filters are used to exclude object members.
+     * 
+     * @see FirePHP->setObjectFilter()
+     * @param string $Class The class name of the object
+     * @param array $Filter An array or members to exclude
+     * @return void
+     */
+    public static function setObjectFilter($Class, $Filter)
+    {
+      $instance = FirePHP::getInstance(true);
+      $instance->setObjectFilter($Class, $Filter);
+    }
   
-  /**
-   * Set some options for the library
-   * 
-   * @see FirePHP->setOptions()
-   * @param array $Options The options to be set
-   * @return void
-   */
-  public static function setOptions($Options) {
-    $instance = FirePHP::getInstance(true);
-    $instance->setOptions($Options);
-  }
+    /**
+     * Set some options for the library
+     * 
+     * @see FirePHP->setOptions()
+     * @param array $Options The options to be set
+     * @return void
+     */
+    public static function setOptions($Options)
+    {
+        $instance = FirePHP::getInstance(true);
+        $instance->setOptions($Options);
+    }
 
-  /**
-   * Get options for the library
-   * 
-   * @see FirePHP->getOptions()
-   * @return array The options
-   */
-  public static function getOptions() {
-    $instance = FirePHP::getInstance(true);
-    return $instance->getOptions();
-  }
+    /**
+     * Get options for the library
+     * 
+     * @see FirePHP->getOptions()
+     * @return array The options
+     */
+    public static function getOptions()
+    {
+        $instance = FirePHP::getInstance(true);
+        return $instance->getOptions();
+    }
 
-  /**
-   * Log object to firebug
-   * 
-   * @see http://www.firephp.org/Wiki/Reference/Fb
-   * @param mixed $Object
-   * @return true
-   * @throws Exception
-   */
-  public static function send()
-  {
-    $instance = FirePHP::getInstance(true);
-    $args = func_get_args();
-    return call_user_func_array(array($instance,'fb'),$args);
-  }
+    /**
+     * Log object to firebug
+     * 
+     * @see http://www.firephp.org/Wiki/Reference/Fb
+     * @param mixed $Object
+     * @return true
+     * @throws Exception
+     */
+    public static function send()
+    {
+        $instance = FirePHP::getInstance(true);
+        $args = func_get_args();
+        return call_user_func_array(array($instance,'fb'),$args);
+    }
 
-  /**
-   * Start a group for following messages
-   * 
-   * Options:
-   *   Collapsed: [true|false]
-   *   Color:     [#RRGGBB|ColorName]
-   *
-   * @param string $Name
-   * @param array $Options OPTIONAL Instructions on how to log the group
-   * @return true
-   */
-  public static function group($Name, $Options=null) {
-    $instance = FirePHP::getInstance(true);
-    return $instance->group($Name, $Options);
-  }
+    /**
+     * Start a group for following messages
+     * 
+     * Options:
+     *   Collapsed: [true|false]
+     *   Color:     [#RRGGBB|ColorName]
+     *
+     * @param string $Name
+     * @param array $Options OPTIONAL Instructions on how to log the group
+     * @return true
+     */
+    public static function group($Name, $Options=null)
+    {
+        $instance = FirePHP::getInstance(true);
+        return $instance->group($Name, $Options);
+    }
 
-  /**
-   * Ends a group you have started before
-   *
-   * @return true
-   * @throws Exception
-   */
-  public static function groupEnd() {
-    return self::send(null, null, FirePHP::GROUP_END);
-  }
+    /**
+     * Ends a group you have started before
+     *
+     * @return true
+     * @throws Exception
+     */
+    public static function groupEnd()
+    {
+        return self::send(null, null, FirePHP::GROUP_END);
+    }
 
-  /**
-   * Log object with label to firebug console
-   *
-   * @see FirePHP::LOG
-   * @param mixes $Object
-   * @param string $Label
-   * @return true
-   * @throws Exception
-   */
-  public static function log($Object, $Label=null) {
-    return self::send($Object, $Label, FirePHP::LOG);
-  } 
+    /**
+     * Log object with label to firebug console
+     *
+     * @see FirePHP::LOG
+     * @param mixes $Object
+     * @param string $Label
+     * @return true
+     * @throws Exception
+     */
+    public static function log($Object, $Label=null)
+    {
+        return self::send($Object, $Label, FirePHP::LOG);
+    } 
 
-  /**
-   * Log object with label to firebug console
-   *
-   * @see FirePHP::INFO
-   * @param mixes $Object
-   * @param string $Label
-   * @return true
-   * @throws Exception
-   */
-  public static function info($Object, $Label=null) {
-    return self::send($Object, $Label, FirePHP::INFO);
-  } 
+    /**
+     * Log object with label to firebug console
+     *
+     * @see FirePHP::INFO
+     * @param mixes $Object
+     * @param string $Label
+     * @return true
+     * @throws Exception
+     */
+    public static function info($Object, $Label=null)
+    {
+        return self::send($Object, $Label, FirePHP::INFO);
+    } 
 
-  /**
-   * Log object with label to firebug console
-   *
-   * @see FirePHP::WARN
-   * @param mixes $Object
-   * @param string $Label
-   * @return true
-   * @throws Exception
-   */
-  public static function warn($Object, $Label=null) {
-    return self::send($Object, $Label, FirePHP::WARN);
-  } 
+    /**
+     * Log object with label to firebug console
+     *
+     * @see FirePHP::WARN
+     * @param mixes $Object
+     * @param string $Label
+     * @return true
+     * @throws Exception
+     */
+    public static function warn($Object, $Label=null)
+    {
+        return self::send($Object, $Label, FirePHP::WARN);
+    } 
 
-  /**
-   * Log object with label to firebug console
-   *
-   * @see FirePHP::ERROR
-   * @param mixes $Object
-   * @param string $Label
-   * @return true
-   * @throws Exception
-   */
-  public static function error($Object, $Label=null) {
-    return self::send($Object, $Label, FirePHP::ERROR);
-  } 
+    /**
+     * Log object with label to firebug console
+     *
+     * @see FirePHP::ERROR
+     * @param mixes $Object
+     * @param string $Label
+     * @return true
+     * @throws Exception
+     */
+    public static function error($Object, $Label=null)
+    {
+        return self::send($Object, $Label, FirePHP::ERROR);
+    } 
 
-  /**
-   * Dumps key and variable to firebug server panel
-   *
-   * @see FirePHP::DUMP
-   * @param string $Key
-   * @param mixed $Variable
-   * @return true
-   * @throws Exception
-   */
-  public static function dump($Key, $Variable) {
-    return self::send($Variable, $Key, FirePHP::DUMP);
-  } 
+    /**
+     * Dumps key and variable to firebug server panel
+     *
+     * @see FirePHP::DUMP
+     * @param string $Key
+     * @param mixed $Variable
+     * @return true
+     * @throws Exception
+     */
+    public static function dump($Key, $Variable)
+    {
+        return self::send($Variable, $Key, FirePHP::DUMP);
+    } 
 
-  /**
-   * Log a trace in the firebug console
-   *
-   * @see FirePHP::TRACE
-   * @param string $Label
-   * @return true
-   * @throws Exception
-   */
-  public static function trace($Label) {
-    return self::send($Label, FirePHP::TRACE);
-  } 
+    /**
+     * Log a trace in the firebug console
+     *
+     * @see FirePHP::TRACE
+     * @param string $Label
+     * @return true
+     * @throws Exception
+     */
+    public static function trace($Label)
+    {
+        return self::send($Label, FirePHP::TRACE);
+    } 
 
-  /**
-   * Log a table in the firebug console
-   *
-   * @see FirePHP::TABLE
-   * @param string $Label
-   * @param string $Table
-   * @return true
-   * @throws Exception
-   */
-  public static function table($Label, $Table) {
-    return self::send($Table, $Label, FirePHP::TABLE);
-  } 
+    /**
+     * Log a table in the firebug console
+     *
+     * @see FirePHP::TABLE
+     * @param string $Label
+     * @param string $Table
+     * @return true
+     * @throws Exception
+     */
+    public static function table($Label, $Table)
+    {
+        return self::send($Table, $Label, FirePHP::TABLE);
+    } 
 
 }
-
