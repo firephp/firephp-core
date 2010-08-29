56e3038da7d15aa423bdff91b9024107191d3fc3
cadorn <christoph@christophdorn.com>
2010-08-29 16:23:19 -0700

    Bugfix: Avoid globals pollution during initialization

--- packages/insight/lib/FirePHP/Init.php
+++ packages/insight/lib/FirePHP/Init.php
@@ -1,59 +1,66 @@
 <?php
 
-// Only activate FirePHP if certain header prefixes are found:
-//  * x-wf-
-//  * x-insight
+// This function is called at the bottom of this page
+function FirePHP__main() {
 
-$__FIREPHP__headers = false;
-if(function_exists('getallheaders')) {
-    $__FIREPHP__headers = getallheaders();
-} else {
-    $__FIREPHP__headers = $_SERVER;
-}
-$__FIREPHP__activate = false;
-foreach( $__FIREPHP__headers as $name => $value ) {
-    $name = strtolower($name);
-    if(substr($name, 0, 5) == 'http_') {
-        $name = str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)));
+    // Only activate FirePHP if certain header prefixes are found:
+    //  * x-wf-
+    //  * x-insight
+    
+    $headers = false;
+    if(function_exists('getallheaders')) {
+        $headers = getallheaders();
+    } else {
+        $headers = $_SERVER;
     }
-    if(substr($name, 0, 5)=='x-wf-') {
-        $__FIREPHP__activate = true;
-    } else
-    if(substr($name, 0, 9)=='x-insight') {
-        $__FIREPHP__activate = true;
-    }
-}
-unset($__FIREPHP__headers);
-
-if($__FIREPHP__activate) {
-
-    unset($__FIREPHP__activate);
-
-    set_include_path(get_include_path() . PATH_SEPARATOR . dirname(dirname(__FILE__)));
-    
-    require_once('FirePHP/Insight.php');
-    
-    FirePHP::setInstance(new FirePHP_Insight());
-    
-    Insight_Helper__main();
-
-} else {
-
-    unset($__FIREPHP__activate);
-
-    class FirePHP {
-        public function getEnabled() {
-            return false;
+    $activate = false;
+    foreach( $headers as $name => $value ) {
+        $name = strtolower($name);
+        if(substr($name, 0, 5) == 'http_') {
+            $name = str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)));
         }
-        public function detectClientExtension() {
-            return false;
-        }
-        public function __call($name, $arguments) {
-            return new FirePHP();
-        }
-        public static function __callStatic($name, $arguments) {
-            return new FirePHP();
+        if(substr($name, 0, 5)=='x-wf-') {
+            $activate = true;
+        } else
+        if(substr($name, 0, 9)=='x-insight') {
+            $activate = true;
         }
     }
-
+    
+    if($activate) {
+    
+        set_include_path(get_include_path() . PATH_SEPARATOR . dirname(dirname(__FILE__)));
+        
+        require_once('FirePHP/Insight.php');
+        
+        FirePHP::setInstance(new FirePHP_Insight());
+        
+        Insight_Helper__main();
+    
+    } else {
+    
+        class FirePHP {
+            protected static $instance = null;
+            public static function getInstance() {
+                if(!self::$instance) {
+                    self::$instance = new FirePHP();
+                }
+                return self::$instance;
+            }
+            public function getEnabled() {
+                return false;
+            }
+            public function detectClientExtension() {
+                return false;
+            }
+            public function __call($name, $arguments) {
+                return self::getInstance();
+            }
+            public static function __callStatic($name, $arguments) {
+                return self::getInstance();
+            }
+        }
+    }
 }
+
+FirePHP__main();
