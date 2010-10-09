a3a89e2d1ba3931c3ca56343220097ea8704d4a5
cadorn <christoph@christophdorn.com>
2010-10-08 21:19:09 -0700

    * Enhancement: Implement FIREPHP_ACTIVATED constant to force-deactivate FirePHP or indicate if activated based on Init.php [10m]

--- packages/insight/lib/FirePHP/Init.php
+++ packages/insight/lib/FirePHP/Init.php
@@ -1,34 +1,47 @@
 <?php
 
-// This function is called at the bottom of this page
+// This function is called at the bottom of this file
 function FirePHP__main() {
 
-    // Only activate FirePHP if certain header prefixes are found:
-    //  * x-wf-
-    //  * x-insight
-    
-    $headers = false;
-    if(function_exists('getallheaders')) {
-        $headers = getallheaders();
-    } else {
-        $headers = $_SERVER;
+    $activate = true;
+
+    if(defined('FIREPHP_ACTIVATED') && constant('FIREPHP_ACTIVATED')===false) {
+        $activate = false;
     }
-    $activate = false;
-    foreach( $headers as $name => $value ) {
-        $name = strtolower($name);
-        if(substr($name, 0, 5) == 'http_') {
-            $name = str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)));
-        }
-        if(substr($name, 0, 5)=='x-wf-') {
-            $activate = true;
-        } else
-        if(substr($name, 0, 9)=='x-insight') {
-            $activate = true;
-        }
-    }
-    
+
     if($activate) {
-    
+
+        // Only activate FirePHP if certain header prefixes are found:
+        //  * x-wf-
+        //  * x-insight
+        
+        $headers = false;
+        if(function_exists('getallheaders')) {
+            $headers = getallheaders();
+        } else {
+            $headers = $_SERVER;
+        }
+        $activate = false;
+        foreach( $headers as $name => $value ) {
+            $name = strtolower($name);
+            if(substr($name, 0, 5) == 'http_') {
+                $name = str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)));
+            }
+            if(substr($name, 0, 5)=='x-wf-') {
+                $activate = true;
+            } else
+            if(substr($name, 0, 9)=='x-insight') {
+                $activate = true;
+            }
+        }
+    }
+
+    if($activate) {
+
+        if(!defined('FIREPHP_ACTIVATED')) {
+            define('FIREPHP_ACTIVATED', true);
+        }
+
         set_include_path(get_include_path() . PATH_SEPARATOR . dirname(dirname(__FILE__)));
         
         require_once('FirePHP/Insight.php');
