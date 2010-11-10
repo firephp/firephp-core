67d9d0b88f6d890f54c40283476e4a7a278ef1ee
cadorn <christoph@christophdorn.com>
2010-11-10 15:05:30 -0800

    More tests

--- programs/standalone/examples/TestRunner/insight-devcomp/PageConsole-Groups.php
+++ programs/standalone/examples/TestRunner/insight-devcomp/PageConsole-Groups.php
@@ -19,7 +19,7 @@ $console->log('Test Group 2');
 $console->log('Test Value 3');
 
     $group4 = $console->group('sub-group-for-test-group-2');
-    $group4->log('Sub-group for Test Group 2');
+    $group4->label('Group Label')->log('Sub-group for Test Group 2');
     $group4->log('Test Value 4');
 
 $group3->close();
@@ -36,19 +36,38 @@ $group5->log('Test Value 5');
     $group7 = $group5->group('sub-group-2-for-test-group-3')->open();
     $group5->log('Sub-group 2 for Test Group 3');
     $group5->log('Test Value 7');
-    
+
         $group6 = $group5->group('sub-group-1-for-sub-group-2-for-test-group-3');
         $group6->log('Sub-group 1 for Sub-group 2 for Test Group 3');
         $group6->log('Test Value 8');
-        
+
         $group8 = $group5->group('sub-group-2-for-sub-group-2-for-test-group-3')->open();
         $group5->log('Sub-group 2 for Sub-group 2 for Test Group 3');
         $group5->log('Test Value 9');
-        
+
             $group9 = $group5->group('sub-group-1-for-sub-group-2-for-sub-group-2-for-test-group-3');
             $group9->log('Sub-group 1 for Sub-group 2 for Sub-group 2 for Test Group 3');
             $group9->log('Test Value 10');
-        
+
         $group8->close();
 
     $group7->close();
+
+
+$group10 = $console->label('Group Label 1')->group('test-group-4', 'Test Group 4');
+$group10->log('Test Value 11');
+
+$group11 = $console->label('Group Label 2')->group('test-group-5', 'Test Group 5')->open();
+$console->log('Test Value 12');
+$group11->close();
+
+$group11 = $console->group('test-group-5', 'Test Group 5')->open();
+$console->log('Test Value 13');
+$group11->close();
+
+$console->group('test-group-5', 'Test Group 5')->log('Test Value 14');
+$console->group('test-group-5')->log('Test Value 15');
+
+
+$group12 = $console->label('Group Label')->group('Framework', 'Framework Environment');
+$group12->label('Log Label')->log('Log line');
---
+++ programs/standalone/examples/TestRunner/insight-devcomp/PageConsole-Info.php
@@ -0,0 +1,5 @@
+<?php
+
+$firephp = FirePHP::plugin("firephp"); 
+
+$firephp->logVersion();
