55ae4c7f0233457a26d78ffc270ac3853c6d8520
cadorn <christoph@christophdorn.com>
2011-05-10 15:37:21 -0700

    expanded group test

--- programs/standalone/examples/TestRunner/insight-devcomp/RequestConsole-Groups.php
+++ programs/standalone/examples/TestRunner/insight-devcomp/RequestConsole-Groups.php
@@ -25,3 +25,32 @@ $console1->log('Message 1');
 $console1->log('Message 2');
 
 $group->close();
+
+
+
+$group = $console1->expand()->group('Group3', 'Group 3 Title')->open();
+
+$console1->log('Message 1');
+
+$group->close();
+
+
+$group = $console1->expand()->group('Group4', 'Group 4 Title')->open();
+
+$console1->log('Message 1');
+    
+    $group = $console1->group('Group41')->open();
+    
+    $console1->log('Group Title');
+    $console1->log('Message 1');
+    $console1->log('Message 2');
+    
+    $console2->log('Message 3');
+    
+    $group->close();
+
+$group->close();
+
+$group = $console1->expand()->group('Group5', 'Group 5 Title')->open();
+
+$group->close();
