99ec4ea7f7fa18c1f1cd61a4ec16b2ae8606b7f3
cadorn <christoph@christophdorn.com>
2011-05-15 14:23:30 -0700

    better error message

--- packages/insight/lib/FirePHP/Plugin/Engine.php
+++ packages/insight/lib/FirePHP/Plugin/Engine.php
@@ -7,27 +7,27 @@ if(!defined('E_USER_DEPRECATED ')) {
 class FirePHP_Plugin_Engine {
     
     public function onError($console, $types = E_ALL) {
-        trigger_error('FirePHP_Plugin_Engine::onError() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::onError() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public function _errorHandler($errno, $errstr, $errfile, $errline, $errcontext) {
-        trigger_error('FirePHP_Plugin_Engine::_errorHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::_errorHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public function onAssertionError($console) {
-        trigger_error('FirePHP_Plugin_Engine::onAssertionError() is DEPRECATED! This functionality is now available via Insight_Plugin_Assertion and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::onAssertionError() is DEPRECATED! This functionality is now available via Insight_Plugin_Assertion and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public function _assertionErrorHandler($file, $line, $code) {
-        trigger_error('FirePHP_Plugin_Engine::_assertionErrorHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Assertion and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::_assertionErrorHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Assertion and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
    
     public function onException($console) {
-        trigger_error('FirePHP_Plugin_Engine::onException() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::onException() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     function _exceptionHandler($exception) {
-        trigger_error('FirePHP_Plugin_Engine::_exceptionHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::_exceptionHandler() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public function handleException($exception, $console=null) {
@@ -35,14 +35,14 @@ class FirePHP_Plugin_Engine {
     }
     
     public function logException($exception) {
-        trigger_error('FirePHP_Plugin_Engine::logException() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::logException() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public static function parseErrorReportingBitmask($bitmask) {
-        trigger_error('FirePHP_Plugin_Engine::parseErrorReportingBitmask() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::parseErrorReportingBitmask() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }
 
     public function errorLabelForNumber($number) {
-        trigger_error('FirePHP_Plugin_Engine::errorLabelForNumber() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default.', E_USER_DEPRECATED);
+        trigger_error('FirePHP_Plugin_Engine::errorLabelForNumber() is DEPRECATED! This functionality is now available via Insight_Plugin_Error and enabled by default. You can remove this call.', E_USER_DEPRECATED);
     }    
 }
