27de0971dadf7823b973e54e4c886ebf9d9a558f
cadorn <christoph@christophdorn.com>
2011-05-30 11:39:38 -0700

    test

--- programs/standalone/examples/TestRunner/insight-devcomp/PageConsole-Errors.php
+++ programs/standalone/examples/TestRunner/insight-devcomp/PageConsole-Errors.php
@@ -5,4 +5,10 @@ trigger_error("Test error");
 $var = false;
 assert('$var===true');
 
+
+// this triggers an E_NOTICE
+$array = array();
+$var1 = $array['test'];
+
+
 throw new Exception('Test Exception');
